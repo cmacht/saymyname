8c49c88b6fb4032b41a830e83a2d6087b43b51a0
tza@jazz <christian.macht@gmail.com>
2025-02-15 22:50:33 +0100

    update todo

--- README.md
+++ README.md
@@ -3,14 +3,25 @@ A Godot game to help me remember names and faces.
 
 ## Todo
 - [x] get keyboard input
-    - input is `upper` but working with `lower` internally
-- [ ] bbcode to highlight current letter
+- [x] dynamically create character
+- [ ] Character
+    - [ ] set origin (for label)
+    - [ ] animate
+    - [ ] move from left to right
+    - [ ] de-spawn & penalty
+    - [ ] swap `head` on instantiation
+- [ ] Label
+    - [ ] position correctly with character
+    - [ ] style with background (bbcode)
+    - [ ] highlight current letter
+    - [ ] `_____` -> `wal___`
 - [ ] load local photos and names 
 - [ ] rotate through names
 
 ## Optional Todos
 - [ ] time limit
 - [ ] animation: scale image and cycle through happy (say!), neutral (my!), angry (name!) shake
+- [ ] score / persistent scoreboard
 
 ## Useful
 - YT Tutorial Typing Game by jmbiv: [Part 1](https://www.youtube.com/watch?v=qRPI_c9qI1o), [Part 2](https://www.youtube.com/watch?v=CoLRvfk6tbk), Part 3
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,12 +1,11 @@
 extends Node2D
 
-var names := ['walter', 'skyler', 'jesse', 'hank']
+var names := ['skyler', 'jesse', 'hank', 'walter']
 var current_input := ''
 var current_letter := ''
 var current_name := ''
 var current_name_index := 0
 var person_scene = preload("res://scenes/person.tscn")
-var person := person_scene.instantiate()
 
 func get_next_name() -> String:
 	current_name = names.pop_back()
@@ -17,17 +16,19 @@ func get_next_name() -> String:
 func update_current_letter():
 	current_letter = current_name[current_name_index].to_upper()
 	
-func create_person():
-	add_child(person)
+func create_person() -> Node2D:
+	var person := person_scene.instantiate()
 	person.position = Vector2(85.0, 520.0)
 	var label_name = RichTextLabel.new()
-	label_name.text = 'hello world'
+	label_name.text = get_next_name()
 	label_name.size = Vector2(100, 40)
 	label_name.position = Vector2(-40, -180)
 	person.add_child(label_name)
+	return person
 	
 func _ready() -> void:
-	create_person()
+	var person = create_person()
+	add_child(person)
 	# display_name.text = get_next_name().to_upper()
 
 ## Get the player's keyboard input
