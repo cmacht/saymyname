c5f201e0fcd26ebdb2f8d72794e75a41f86f4405
tza@jazz <christian.macht@gmail.com>
2025-02-16 01:50:11 +0100

    wip: constructor for person

---
+++ scenes/person.gd
@@ -0,0 +1,12 @@
+class_name Person extends Node2D
+
+
+
+func _init() -> void:
+	# person.position = Vector2(85.0, 520.0)
+	# var label_name = RichTextLabel.new()
+	# label_name.text = get_next_name()
+	# label_name.size = Vector2(100, 40)
+	# label_name.position = Vector2(-40, -180)
+	# add_child(label_name)
+	pass
--- scripts/main.gd
+++ scripts/main.gd
@@ -5,7 +5,6 @@ var current_input := ''
 var current_letter := ''
 var current_name := ''
 var current_name_index := 0
-var person_scene = preload("res://scenes/person.tscn")
 
 func get_next_name() -> String:
 	current_name = names.pop_back()
@@ -16,19 +15,11 @@ func get_next_name() -> String:
 func update_current_letter():
 	current_letter = current_name[current_name_index].to_upper()
 	
-func create_person() -> Node2D:
-	var person := person_scene.instantiate()
-	person.position = Vector2(85.0, 520.0)
-	var label_name = RichTextLabel.new()
-	label_name.text = get_next_name()
-	label_name.size = Vector2(100, 40)
-	label_name.position = Vector2(-40, -180)
-	person.add_child(label_name)
-	return person
-	
+
 func _ready() -> void:
-	var person = create_person()
+	var person = Person.new()
 	add_child(person)
+	person.position = Vector2(100, 500)
 	# display_name.text = get_next_name().to_upper()
 
 ## Get the player's keyboard input
